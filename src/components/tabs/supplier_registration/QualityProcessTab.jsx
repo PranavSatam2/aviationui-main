a7545f08d7b00e44522f9e15bfed1e9e8f8b8326
PranavSatam2 <125157199+PranavSatam2@users.noreply.github.com>
2025-02-07 00:19:20 +0530

    Add SupplierRegistration component with multi-step form; include tabs for General, Incoming Inspection, Quality Process, Supplier Analysis, and Other

---
+++ src/components/tabs/supplier_registration/QualityProcessTab.jsx
@@ -0,0 +1,12 @@
+
+
+const QualityProcessTab = () =>
+{
+    return (
+        <fieldset className="border border-dark rounded-lg m-2 p-2" style={{height: '380px', overflowY: 'auto'}}>
+            <legend className="w-auto text-muted fs-5 px-2" style={{fontWeight: 'bold', fontSize: '1rem'}}>Quality Process</legend>
+        </fieldset>
+    )
+}
+
+export default QualityProcessTab
