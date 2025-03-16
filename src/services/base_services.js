4ac4eb9bcf79825cfaae437ab08a3a0499c75859
aviation-web <aniket.shinde.dev01@gmail.com>
2025-03-16 16:00:34 +0530

    status

--- src/services/base_services.js
+++ src/services/base_services.js
@@ -1,5 +1,5 @@
 // BASE URL of server
-export const REST_API_BASE_URL = "http://localhost:8082"
+export const REST_API_BASE_URL = "http://localhost:8080"
 
 
 // ############################## URL_SAMPLE ##########################
