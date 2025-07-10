34bb92113311f98d2bc1d60332ce77481c7b9b30
PranavSatam2 <125157199+PranavSatam2@users.noreply.github.com>
2025-07-10 23:29:09 +0530

    Changes Done in material requisition

--- src/components/MaterialRequisition/AddMaterialRequisition/AddMaterialRequisition.jsx
+++ src/components/MaterialRequisition/AddMaterialRequisition/AddMaterialRequisition.jsx
@@ -17,6 +17,7 @@ const AddRequisition = () => {
     issueQty: "",
     issuedQty: "",
     batchLotNo: "",
+    unitOfMeasurement: "",
   });
 
   const handleChange = (e) => {
@@ -125,8 +126,8 @@ const AddRequisition = () => {
 
     // If all validation passes, proceed with submitting
     try {
-        const response = await createMaterialRequisition(form);
-        console.log("Requisition added successfully:", response.data);
+      const response = await createMaterialRequisition(form);
+      console.log("Requisition added successfully:", response.data);
       alert("Requisition Added Successfully!");
       // location.reload();
 
@@ -307,10 +308,36 @@ const AddRequisition = () => {
                       </div>
                     </div>
 
-                    <div className="col-md-12 text-end m-1 p-4 text-right">
-                      <button type="submit" className="btn btn-primary">
-                        Add Requisition
-                      </button>
+                    <div className="col-md-12 d-flex">
+                        <div className="col-md-6 p-2 d-flex">
+                          <label className="col-md-4 mt-2">Unit of Measurement</label>
+                          <select
+                            className="form-select w-100"
+                            name="unitOfMeasurement"
+                            value={form.unitOfMeasurement}
+                            onChange={handleChange}
+                            required
+                          >
+                            <option value="">Select Unit</option>
+                            <option value="EA">EA</option>
+                            <option value="RL">RL</option>
+                            <option value="QT">QT</option>
+                            <option value="GAL">GAL</option>
+                            <option value="KIT">KIT</option>
+                            <option value="LTR">LTR</option>
+                            <option value="SHT">SHT</option>
+                            <option value="Sq.ft">Sq.ft</option>
+                            <option value="Sq.mtr">Sq.mtr</option>
+                          </select>
+                        </div>
+
+                        <div className="col-md-6 p-2 d-flex text-end">
+                          <div className="col-md-4 mt-2 text-end">
+                            <button type="submit" className="btn btn-primary">
+                          Add Requisition
+                        </button>
+                          </div>
+                      </div>
                     </div>
                   </form>
                 </div>
